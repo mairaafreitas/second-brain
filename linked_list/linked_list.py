28e6db40bf476961b8f4adde050d81b1a334bd17
mairaafreitas <maira.oliveirafreitas@gmail.com>
2022-07-10 11:05:41 -0300

    linked list

---
+++ linked_list/linked_list.py
@@ -0,0 +1,127 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if not self.length:
+            return None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            pre = self.head
+            temp = self.head
+            while temp.next:
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+        self.length -= 1
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if not self.length:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+        return True
+
+    def pop_first(self):
+        if not self.length:
+            return None
+        if self.length == 1:
+            self.tail = None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+        self.length -= 1
+
+        return temp
+
+    def get(self, index):
+        temp = self.head
+        while not index:
+            node = temp.next
+
+        print(node)
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if not index:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index > self.length:
+            return None
+        if not index:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
