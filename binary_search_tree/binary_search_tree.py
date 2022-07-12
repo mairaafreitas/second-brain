d341f0aaf2b673f0c0276378a72bda0ceb3571d0
mairaafreitas <maira.oliveirafreitas@gmail.com>
2022-07-12 08:52:13 -0300

    binary search tree min value method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -39,3 +39,9 @@ class BinarySearchTree:
             else:
                 return True
         return False
+
+    def min_value_node(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+
+        return current_node
