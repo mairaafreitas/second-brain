9c39aa68674313f053b069750eb1e46a5238b553
mairaafreitas <maira.oliveirafreitas@gmail.com>
2022-12-09 17:25:35 -0300

    feature: add exercise models

--- django-project/gymtracker/apps/exercise/models.py
+++ django-project/gymtracker/apps/exercise/models.py
@@ -1,3 +1,10 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField(verbose_name="Nome do exercício", max_length=80, null=False, blank=False)
+    repetition = models.IntegerField(verbose_name="Repetições", null=False, blank=False,
+                                     validators=MinValueValidator(0))
+    series = models.IntegerField(verbose_name="Repetições", null=False, blank=False, validators=MinValueValidator(0))
+    load = models.IntegerField(verbose_name="Repetições", null=False, blank=False, default=0)
